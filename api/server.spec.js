c13c17dd551e1c4ea8f169b25185bfea59803365
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-20 10:53:42 -0500

    Completed tests for register api

--- api/server.spec.js
+++ api/server.spec.js
@@ -14,4 +14,24 @@ describe("server", () => {
     it('testing running with DB_ENV = "testing" ', () => {
       expect(process.env.DB_ENV).toBe("testing");
     });
+
+    describe("POST /api/auth/register", () => {
+        it("returns status 201", () => {
+            return request(server)
+              .post('/api/auth/register')
+              .send({username:'potato', password: 'potato'})
+              .then(res => {
+                  expect(res.status).toBe(201);
+              });
+        });
+
+        it("should insert a user into the db", () => {
+            return request(server)
+              .post('/api/auth/register')
+              .send({username:'potato', password: 'potato'})
+              .then(res => {
+                  expect(res.body.length).toBe(1);
+              });
+        });
+    });
 });
